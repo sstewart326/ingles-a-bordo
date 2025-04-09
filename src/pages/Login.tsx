587c4bebf9db9f5736164728334954800c0e1f81
Shawn Stewart <sstewart32619@gmail.com>
2025-04-09 16:59:59 -0300

    google icon

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -197,12 +197,11 @@ export const Login = () => {
               disabled={loading}
               className="w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50"
             >
-              <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24">
-                <path
-                  d="M12.545,10.239v3.821h5.445c-0.712,2.315-2.647,3.972-5.445,3.972c-3.332,0-6.033-2.701-6.033-6.032s2.701-6.032,6.033-6.032c1.498,0,2.866,0.549,3.921,1.453l2.814-2.814C17.503,2.988,15.139,2,12.545,2C7.021,2,2.543,6.477,2.543,12s4.478,10,10.002,10c8.396,0,10.249-7.85,9.426-11.748L12.545,10.239z"
-                  fill="currentColor"
-                />
-              </svg>
+              <img 
+                src="https://developers.google.com/identity/images/g-logo.png" 
+                alt="Google logo" 
+                className="w-5 h-5 mr-2"
+              />
               Sign in with Google
             </button>
           </div>
